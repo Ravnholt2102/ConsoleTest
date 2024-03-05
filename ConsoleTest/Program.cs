8a93ab2da47073d65f201f894cb01d2ab37acbcf
Stefan Nielsen <Stefan.Nielsen@matchware.com>
2024-03-05 13:15:56 +0100

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CefSharp;
+using CefSharp.DevTools.Page;
+using CefSharp.OffScreen;
+using System.Drawing;
+
+Console.WriteLine("Hello, World!");
+
+var settings = new CefSettings() {
+    CachePath = Path.Combine(Path.GetTempPath(), "cefsharpcache")
+};
+await Cef.InitializeAsync(settings);
+
+string url = "www.bing.com";
+BrowserSettings browserSettings = new BrowserSettings {
+    WindowlessFrameRate = 1,
+};
+using (ChromiumWebBrowser browser = new ChromiumWebBrowser(url, browserSettings)) {
+    browser.Size = new Size(1920, 1080);
+    byte[] screenshot;
+
+    await browser.WaitForRenderIdleAsync();
+    screenshot = await browser.CaptureScreenshotAsync(CaptureScreenshotFormat.Png);
+    await File.WriteAllBytesAsync(Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".png"), screenshot);
+}
